6614856ee8d3e00de8ea5b06f83fa3500bfe90b6
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-17 18:23:14 +0200

    Control Statements: While Loops

---
+++ Control Statements While Loops/index.js
@@ -0,0 +1,29 @@
+
+// My Solution
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+    //write code here.
+    while (count <= 100) {
+
+
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz");
+        }
+        else if (count % 3 === 0) {
+            output.push("Fizz");
+        }
+        else if (count % 5 === 0) {
+            output.push("Buzz");
+        }
+        else {
+            output.push(count);
+        }
+        count++;
+
+    }
+    console.log(count);
+}
+fizzBuzz();
