f52bfbc5afb3fdaca5f6a6c78b811b0216340152
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-17 16:46:45 +0200

    Collections: Working with Javascript Arrays

---
+++ Collections Working with Javascript Arrays/index.js
@@ -0,0 +1,25 @@
+
+// My Solution
+
+var guesList = ["Angela", "Jac", "Pam", "Lara", "James", "Jason"];
+
+var guesname = prompt("What is your Name?")
+
+if (guesList.includes(guesname)) {
+    alert("You are welcome")
+} else {
+    alert("Sorry next time")
+}
+
+
+
+//Angela Solution
+
+
+var guesName = prompt("What is your Name?")
+
+if (guesList.includes(guesName)) {
+    alert("You are welcome!")
+} else {
+    alert("Sorry next time")
+}
